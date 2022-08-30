9a0343b0df38227602f466b44bf139f4f7ecc7ef
Gustavo Willam <42176577+gustavowillam@users.noreply.github.com>
2022-08-30 11:56:52 -0300

    Add files via upload

---
+++ programas/HelloWordJOptionPane.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+public class HelloWordJOptionPane
+{
+   public static void main(String[] args) 
+   {
+         //System.out.println("Olá Mundo");  
+         JOptionPane.showMessageDialog(null, "Olá Mundo");
+   }
+
+}
+
